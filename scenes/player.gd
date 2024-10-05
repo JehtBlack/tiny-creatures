50431b541562ba603abfc47db474da147822ca95
BetterThanAI <thomaslashbrook@gmail.com>
2024-10-05 10:28:08 +0100

    Better Controls

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,10 +3,21 @@ extends CharacterBody2D
 
 @export var speed = 400
 
-func get_input():
-	look_at(get_global_mouse_position())
-	velocity = transform.x * Input.get_axis("Backwards", "Forwards") * speed
+var look = true
 
+func get_input():
+	if look == true:
+		look_at(get_global_mouse_position())
+	var input_direction = Input.get_vector("Left", "Right", "Forwards", "Backwards")
+	velocity = input_direction * speed
+		
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+
+func _on_control_mouse_entered():
+	look = false
+	
+func _on_control_mouse_exited():
+	look = true
