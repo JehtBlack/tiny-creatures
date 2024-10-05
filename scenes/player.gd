01def70a3e178225dfe40f6107ba1ee379a93666
BetterThanAI <thomaslashbrook@gmail.com>
2024-10-05 11:21:28 +0100

    Bullets and shooting added

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 @export var speed = 400
+@export var Bullet : PackedScene
 
 var look = true
 
@@ -10,6 +11,9 @@ func get_input():
 		look_at(get_global_mouse_position())
 	var input_direction = Input.get_vector("Left", "Right", "Forward", "Backward")
 	velocity = input_direction * speed
+	if Input.is_action_just_pressed("Shoot"):
+		shoot()
+
 		
 func _physics_process(delta):
 	get_input()
@@ -21,3 +25,9 @@ func _on_control_mouse_entered():
 	
 func _on_control_mouse_exited():
 	look = true
+
+
+func shoot():
+	var b = Bullet.instantiate()
+	owner.add_child(b)
+	b.transform = $Muzzle.global_transform
