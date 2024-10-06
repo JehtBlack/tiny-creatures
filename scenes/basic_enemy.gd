f6443e982d026cc711cd2ac20030a82699705043
JehtBlack <jehtblack7@gmail.com>
2024-10-06 12:55:46 +0100

    Added a load a navigation work
    glocklins wander and avoid the player
    enemies have wander and basic pursuit code, may not be working properly

--- scenes/basic_enemy.gd
+++ scenes/basic_enemy.gd
@@ -4,24 +4,50 @@ var run_speed = 50
 var health = 10
 var player = null
 
-func _physics_process(_delta):
-	velocity = Vector2.ZERO
+var wait_timer: Timer
+var wait_time_min: float = 0.5
+var wait_time_max: float = 2.0
+
+func _ready() -> void:
+	wait_timer = Timer.new()
+	add_child(wait_timer)
+	wait_timer.one_shot = true
+	wait_timer.timeout.connect(pick_new_target)
+
+	NavigationServer2D.map_changed.connect(_on_nav_map_changed)
+
+func pick_new_target() -> void:
 	if player != null:
-		velocity = position.direction_to(player.position) * run_speed
-		look_at(player.position)
+		$NavigationAgent2D.target_position = player.position
+	else:
+		if wait_timer.is_stopped():
+			wait_timer.wait_time = randf_range(wait_time_min, wait_time_max)
+			wait_timer.start()
+			var target_point = NavigationServer2D.map_get_random_point($NavigationAgent2D.get_navigation_map(), 1, false)
+			$NavigationAgent2D.target_position = target_point
+
+func _physics_process(_delta):
+	var direction = ($NavigationAgent2D.get_next_path_position() - global_position).normalized()
+	velocity = direction * run_speed
+	rotation = velocity.angle()
 	move_and_slide()
 
 
 func _on_detect_area_body_entered(body: Node2D) -> void:
-	player = body
-	print("I heard a noise!")
+	if body.is_in_group("player"):
+		player = body
+		print("I heard a noise!")
 
 
-func _on_detect_area_body_exited(_body: Node2D) -> void:
-	player = null
-	print("Must be the wind...")
+func _on_detect_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = null
+		print("Must be the wind...")
 
 func hit(damage):
 	health = health - damage
 	if health <= 0:
 		queue_free()
+
+func _on_nav_map_changed(_new_map: RID):
+	pick_new_target()
--- scenes/glocklin.gd
+++ scenes/glocklin.gd
@@ -4,52 +4,45 @@ extends CharacterBody2D
 
 var run_speed = 50
 var health = 10
-var player = null
-var wander_timer: Timer
-var wander_time_min: float = 0.2
-var wander_time_max: float = 2.0
+
+var wait_timer: Timer
+var min_wait_time: float = 0.5
+var max_wait_time: float = 2.0
 
 func _ready() -> void:
-	wander_timer = Timer.new()
-	add_child(wander_timer)
-	wander_timer.one_shot = true
-	_adjust_wander_time()
+	wait_timer = Timer.new()
+	add_child(wait_timer)
+	wait_timer.one_shot = true
+	wait_timer.timeout.connect(pick_new_target)
 
-func _adjust_wander_time():
-	wander_timer.wait_time = randf_range(wander_time_min, wander_time_max)
+	NavigationServer2D.map_changed.connect(_on_nav_map_changed)
 
-func wander():
-	if player == null:
-		if wander_timer.is_stopped():
-			_adjust_wander_time()
-			wander_timer.start()
-			var direction = Vector2.RIGHT.rotated(randf_range(0.0, PI * 2))
-			velocity = direction * (run_speed / 2.0)
-			look_at(direction)
+func pick_new_target():
+	var target_point = NavigationServer2D.map_get_random_point($NavigationAgent2D.get_navigation_map(), 1, false)
+	$NavigationAgent2D.target_position = target_point
 
 func _physics_process(_delta):
-	if player != null:
-		var direction = position.direction_to(player.position)
-		direction = direction.rotated(PI)
-		velocity = direction * run_speed
-		look_at(direction)
-	wander()
+	var next_path_pos: Vector2 = $NavigationAgent2D.get_next_path_position()
+	var direction: Vector2 = (next_path_pos - global_position).normalized()
+	$NavigationAgent2D.velocity = direction * run_speed
 	move_and_slide()
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = body
-		print("Glocklin being chased")
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player = null
-		print("You picked the wrong house fool!")
 
 
 func _on_area_pickup_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body._add_gun(gun)
 		queue_free()
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+	rotation = velocity.angle()
+
+
+func _on_navigation_agent_2d_target_reached() -> void:
+	if wait_timer.is_stopped():
+		wait_timer.wait_time = randf_range(min_wait_time, max_wait_time)
+		wait_timer.start()
+
+func _on_nav_map_changed(_new_map: RID):
+	pick_new_target()
