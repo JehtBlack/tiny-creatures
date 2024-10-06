428d9ed46726f036c9896b087535e84ff813894b
JehtBlack <jehtblack7@gmail.com>
2024-10-06 13:17:15 +0100

    Fix enemy nav code

--- scenes/basic_enemy.gd
+++ scenes/basic_enemy.gd
@@ -8,18 +8,25 @@ var wait_timer: Timer
 var wait_time_min: float = 0.5
 var wait_time_max: float = 2.0
 
+var player_tracking_timer: Timer
+var player_tracking_time: float = 0.5
+
 func _ready() -> void:
 	wait_timer = Timer.new()
 	add_child(wait_timer)
 	wait_timer.one_shot = true
 	wait_timer.timeout.connect(pick_new_target)
 
+	player_tracking_timer = Timer.new()
+	add_child(player_tracking_timer)
+	player_tracking_timer.one_shot = false
+	player_tracking_timer.wait_time = player_tracking_time
+	player_tracking_timer.timeout.connect(_track_player)
+
 	NavigationServer2D.map_changed.connect(_on_nav_map_changed)
 
 func pick_new_target() -> void:
-	if player != null:
-		$NavigationAgent2D.target_position = player.position
-	else:
+	if player == null:
 		if wait_timer.is_stopped():
 			wait_timer.wait_time = randf_range(wait_time_min, wait_time_max)
 			wait_timer.start()
@@ -29,19 +36,25 @@ func pick_new_target() -> void:
 func _physics_process(_delta):
 	var direction = ($NavigationAgent2D.get_next_path_position() - global_position).normalized()
 	velocity = direction * run_speed
-	rotation = velocity.angle()
+	rotation = velocity.angle() + (PI / 2)
 	move_and_slide()
 
 
 func _on_detect_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player = body
+		player_tracking_timer.start()
+		_track_player()
 		print("I heard a noise!")
 
 
 func _on_detect_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		player_tracking_timer.stop()
 		player = null
+		$NavigationAgent2D.target_position = global_position
+		wait_timer.wait_time = randf_range(wait_time_min, wait_time_max)
+		wait_timer.start()
 		print("Must be the wind...")
 
 func hit(damage):
@@ -49,5 +62,13 @@ func hit(damage):
 	if health <= 0:
 		queue_free()
 
+func _track_player():
+	if player != null:
+		$NavigationAgent2D.target_position = player.position
+
 func _on_nav_map_changed(_new_map: RID):
+	pick_new_target()
+
+
+func _on_navigation_agent_2d_target_reached() -> void:
 	pick_new_target()
